2e7e65b18b5e0166cd0abcfe1754a497b576ccd5
Andy Donnell <a.donnell@elsevier.com>
2017-06-26 11:20:39 -0400

    Format files for readability

--- src/main/java/com/qa/controllers/AddressBookController.java
+++ src/main/java/com/qa/controllers/AddressBookController.java
@@ -15,64 +15,51 @@ public class AddressBookController {
 
 	@Autowired
 	private AddressService addressService;
-	
-	
+
 	@RequestMapping("/updateAddress")
-	public ModelAndView updateAddress(@ModelAttribute("logged_in_customer") Customer loggedInCustomer, @ModelAttribute("Address") Address address)
-	{
-		
-		ModelAndView modelAndView  = null;
-		
+	public ModelAndView updateAddress(@ModelAttribute("logged_in_customer") Customer loggedInCustomer,
+			@ModelAttribute("Address") Address address) {
+
+		ModelAndView modelAndView = null;
+
 		Address billingAddress = null;
 		Address shippingAddress = null;
 		System.out.println("Before update ");
 
-		System.out.println("ID "+loggedInCustomer.getCustomerId());
-		System.out.println("Name"+loggedInCustomer.getFirstName());
-		System.out.println("Email"+loggedInCustomer.getEmail());
-		
-		
+		System.out.println("ID " + loggedInCustomer.getCustomerId());
+		System.out.println("Name" + loggedInCustomer.getFirstName());
+		System.out.println("Email" + loggedInCustomer.getEmail());
+
 		Address bAddress = addressService.findAddressByType(loggedInCustomer.getCustomerId(), "billing");
-		
+
 		Address sAddress = addressService.findAddressByType(loggedInCustomer.getCustomerId(), "shipping");
-		
-		if(bAddress!=null || sAddress!=null)
-		{
-		
-		        int recordsUpdated = addressService.updateBillingAddress(address.getAddressLine1(),
-				address.getAddressLine2(), 
-				address.getCity(), 
-				address.getPostcode(),
-				address.getState(),
-				address.getCountry(), 
-				address.getPhoneNumber(), 
-				loggedInCustomer.getCustomerId(), 
-				address.getAddressType());
-		
-		if(recordsUpdated>0)
-		{
-			billingAddress  = addressService.findAddressByType(loggedInCustomer.getCustomerId(),"billing");
-			shippingAddress  = addressService.findAddressByType(loggedInCustomer.getCustomerId(),"shipping");
-			
-			System.out.println("After update ");
-			modelAndView = new ModelAndView("address_book","billing_address",billingAddress);
-			modelAndView.addObject("shipping_address", shippingAddress);
-		}
-		else
-		{
-			modelAndView = new ModelAndView("address_book","billing_address",billingAddress);
-			modelAndView.addObject("shipping_address", shippingAddress);
-			
-		}
-		
-		}
-		else
-		{
+
+		if (bAddress != null || sAddress != null) {
+
+			int recordsUpdated = addressService.updateBillingAddress(address.getAddressLine1(),
+					address.getAddressLine2(), address.getCity(), address.getPostcode(), address.getState(),
+					address.getCountry(), address.getPhoneNumber(), loggedInCustomer.getCustomerId(),
+					address.getAddressType());
+
+			if (recordsUpdated > 0) {
+				billingAddress = addressService.findAddressByType(loggedInCustomer.getCustomerId(), "billing");
+				shippingAddress = addressService.findAddressByType(loggedInCustomer.getCustomerId(), "shipping");
+
+				System.out.println("After update ");
+				modelAndView = new ModelAndView("address_book", "billing_address", billingAddress);
+				modelAndView.addObject("shipping_address", shippingAddress);
+			} else {
+				modelAndView = new ModelAndView("address_book", "billing_address", billingAddress);
+				modelAndView.addObject("shipping_address", shippingAddress);
+
+			}
+
+		} else {
 			Address savedAddress = addressService.save(address);
-			modelAndView = new ModelAndView("address_book","billing_address",savedAddress);
-			
+			modelAndView = new ModelAndView("address_book", "billing_address", savedAddress);
+
 		}
 		return modelAndView;
 	}
-	
+
 }
