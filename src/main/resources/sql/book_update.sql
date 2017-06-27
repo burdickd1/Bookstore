e02646cb1c72ee230cbef59e5b5b3ed162e6fade
Andy Donnell <a.donnell@elsevier.com>
2017-06-27 16:50:17 -0400

    Add book_update.sql - updates fields to correct images

---
+++ src/main/resources/sql/book_update.sql
@@ -0,0 +1,45 @@
+UPDATE book
+SET book.book_image = 'images/book_covers/1.jpg', book.description='This text serves as an introduction to the programming language Java for scientists and engineers, as well as experienced programmers wishing to learn Java as an additional language.', book.title='Essential Java For Scientists and Engineers'
+WHERE book.book_id=1;
+UPDATE book
+SET book.book_image = 'images/book_covers/2.jpg', book.description='A Dance with Dragons is the fifth of seven planned novels in the epic fantasy series A Song of Ice and Fire by American author George R. R. Martin.', book.title='A Dance With Dragons'
+WHERE book.book_id=2;
+UPDATE book
+SET book.book_image = 'images/book_covers/3.jpg', book.description='When fresh eggs are needed for the kitchen, what could be better than going out to the back yard and collecting them, courtesy of home-bred chickens? Food simply doesn\'t come any fresher, and the good news is that keeping two or three chickens is quite easy.', book.title='Keeping Chickens as Pets'
+WHERE book.book_id=3;
+UPDATE book
+SET book.book_image = 'images/book_covers/4.jpg', book.description='Astrophysics for People in a Hurry is a 2017 popular science book by Neil deGrasse Tyson, centering around a number of basic questions about the universe.', book.title='Astrophysics for People in a Hurry'
+WHERE book.book_id=4;
+UPDATE book
+SET book.book_image = 'images/book_covers/5.jpg', book.description='Elin Hilderbrand has a twin brother who is not a bestselling novelist. She does her best writing on the beaches of Nantucket as well as on the charming streets of Beacon Hill, in Boston. She has three magical children who beg her not to sing along to the radio or dance in public. The Identicals is her nineteenth novel.', book.title='The Identicals'
+WHERE book.book_id=5;
+UPDATE book
+SET book.book_image = 'images/book_covers/6.jpg', book.description='', book.title=''
+WHERE book.book_id=6;
+UPDATE book
+SET book.book_image = 'images/book_covers/7.jpg', book.description='', book.title=''
+WHERE book.book_id=7;
+UPDATE book
+SET book.book_image = 'images/book_covers/8.jpg', book.description='', book.title=''
+WHERE book.book_id=8;
+UPDATE book
+SET book.book_image = 'images/book_covers/9.jpg', book.description='', book.title=''
+WHERE book.book_id=9;
+UPDATE book
+SET book.book_image = 'images/book_covers/10.jpg', book.description='', book.title=''
+WHERE book.book_id=10;
+UPDATE book
+SET book.book_image = 'images/book_covers/11.jpg', book.description='', book.title=''
+WHERE book.book_id=11;
+UPDATE book
+SET book.book_image = 'images/book_covers/12.jpg', book.description='', book.title=''
+WHERE book.book_id=12;
+UPDATE book
+SET book.book_image = 'images/book_covers/13.jpg', book.description='', book.title=''
+WHERE book.book_id=13;
+UPDATE book
+SET book.book_image = 'images/book_covers/14.jpg', book.description='', book.title=''
+WHERE book.book_id=14;
+UPDATE book
+SET book.book_image = 'images/book_covers/15.jpg', book.description='', book.title=''
+WHERE book.book_id=15;
