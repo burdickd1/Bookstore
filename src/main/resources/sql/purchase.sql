8378ff4d2efe00befa8c92942669c35997d44738
meslerke <meslerke@miamioh.edu>
2017-06-29 15:32:54 -0400

    added tests and purchase.sql

---
+++ src/main/resources/sql/purchase.sql
@@ -0,0 +1,3 @@
+use elsevier;
+
+INSERT IGNORE INTO purchase (book_id_purchase, customer_id_purchase, date, quantity) values (318, 10, '2017/05/12', 7);
--- src/test/java/com/qa/ElsevierProjectApplicationTests.java
+++ src/test/java/com/qa/ElsevierProjectApplicationTests.java
@@ -1,29 +1,59 @@
 package com.qa;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.qa.controllers.BookController;
+import com.qa.models.Address;
 import com.qa.models.Book;
+import com.qa.models.Customer;
+import com.qa.models.Purchase;
+import com.qa.services.AddressService;
+import com.qa.services.BookService;
+import com.qa.services.CustomerService;
+import com.qa.services.OrderService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ElsevierProjectApplicationTests {
+//	@Autowired
+//	private BookService bs;
+	
+	@Autowired
+	private CustomerService cs;
+	Customer c;
+	
+	@Autowired
+	private AddressService as;
+	Address address;
+	
+	@Autowired
+	private OrderService os;
+	
+	List<Book> books;
+	//Book b1, b2, b3, b4;
+	
+	List<Purchase> purchases;
+	Purchase p;
+	
 	private BookController bc;
 	ArrayList<Book> cartItems;
 	ArrayList<Integer> cartItemIds;
-	ArrayList<Book> books;
 	//Iterable<Book> books;
 	ArrayList<Integer> bookIds;
 	int bookId;
@@ -32,6 +62,17 @@ public class ElsevierProjectApplicationTests {
 	
 	@Before
 	public void setUp() {
+//		b1 = bs.findOne(3);
+//		b2 = bs.findOne(6);
+//		b3 = bs.findOne(10);
+//		b4 = bs.findOne(13);
+//		books = new ArrayList<Book>(Arrays.asList(b1, b2, b3, b4));
+		
+		c = cs.findOne(10);	
+		address = as.findOne(1);
+		p = os.findOne(0);
+		purchases = new ArrayList<Purchase>(Arrays.asList(p));
+		
 		bc = new BookController();
 		
 		b0 = new Book();
@@ -68,6 +109,54 @@ public class ElsevierProjectApplicationTests {
 	   System.out.println("teardown");
 	}
 	
+	/*
+	//BookService
+	@Test
+	public void testSearchBooks() {
+		List<Book> test = bs.searchBooks("chicken"); //LazyInitializationException
+		assertEquals(test, books);
+	}
+	*/
+	
+	//Customer Service
+	@Test
+	public void testLoginProcess() {
+		Customer c1 = cs.loginProcess(c.getEmail(), c.getPassword());
+		assertEquals(c1.getCustomerId(), c.getCustomerId());
+	}
+	
+	@Test
+	public void testUpdateCustomer() {
+		assertEquals(cs.updateCustomer(c.getFirstName(), c.getLastName(), c.getEmail(), c.getCustomerId()), 1);
+	}
+	
+	@Test
+	public void testUpdatePassword() {
+		assertEquals(cs.updatePassword(c.getPassword(), c.getCustomerId()), 1);
+	}
+	
+	//AddressService
+	@Test
+	public void testUpdateBillingAddress() {
+		int i = as.updateBillingAddress(address.getAddressLine1(), address.getAddressLine2(), address.getCity(),
+										address.getPostcode(), address.getState(), address.getCountry(), 
+										address.getPhoneNumber(), address.getCustomerId(), address.getAddressType());
+		assertEquals(i, 1);
+	}
+	
+	@Test
+	public void testFindAddressByType() {
+		Address test = as.findAddressByType(address.getCustomerId(), address.getAddressType());
+		assertEquals(test.getCustomerId(), address.getCustomerId());
+	}
+	
+	//OrderService
+	@Test
+	public void testFindByCustomerId() {
+		assertEquals(os.findByCustomerId(c.getCustomerId()).get(0).getCustomerIdPurchase(), purchases.get(0).getCustomerIdPurchase());
+	}
+	
+	//Book Controller
 	@Test
 	public void testLoadBookIds() {
 		assertEquals(bc.loadBookIds(cartItems), cartItemIds);
@@ -89,7 +178,7 @@ public class ElsevierProjectApplicationTests {
 	public void testRemoveBookById() {
 		bookId = 3;
 		ArrayList<Book> test = new ArrayList(Arrays.asList(b0, b1, b2, b4, b2));
-		assertEquals(bc.removeBookById(books, bookId), test);	
+		assertEquals(bc.removeBookById((ArrayList<Book>) books, bookId), test);	
 	}
 	
 	@Test
@@ -101,5 +190,117 @@ public class ElsevierProjectApplicationTests {
 	public void testFilteredBookList() {
 		assertEquals(bc.filteredBookList(books, map), books);
 	}
-
+	
+	//Purchase
+	@Test
+	public void testGetPurchaseId() {
+		assertEquals(p.getPurchaseId(), 0);
+	}
+	
+	@Test
+	public void testGetBookIdPurchase() {
+		assertEquals(p.getBookIdPurchase(), 318);
+	}
+	
+	@Test
+	public void testSetBookIdPurchase() {
+		p.setBookIdPurchase(50);
+		assertEquals(p.getBookIdPurchase(), 50);
+	}
+	
+	@Test
+	public void testGetCustomerIdPurchase() {
+		assertEquals(p.getCustomerIdPurchase(), 10);
+	}
+	
+	@Test
+	public void testSetCustomerIdPurchase() {
+		p.setCustomerIdPurchase(400);
+		assertEquals(p.getCustomerIdPurchase(), 400);
+	}
+	
+	@Test
+	public void testGetDate() {
+		assertEquals(os.findByCustomerId(c.getCustomerId()).get(0).getDate(), purchases.get(0).getDate());
+	}
+	
+	@Test
+	public void testSetDate() {
+		Date date = new Date();
+		p.setDate(date);
+		assertEquals(p.getDate(), date);
+	}
+	
+	@Test
+	public void testGetQuantity() {
+		assertEquals(p.getQuantity(), 7);
+	}
+	
+	@Test
+	public void testSetQuantity() {
+		p.setQuantity(40);
+		assertEquals(p.getQuantity(), 40);
+	}
+	
+	@Test
+	public void testPurchaseToString() {
+		assertEquals(p.toString(), "Purchase{bookIdPurchase=318, customerIdPurchase=10, date=" + p.getDate() + ", quantity=7}");
+	}
+	
+	//Customer
+	@Test
+	public void testGetCustomerId() {
+		assertEquals(c.getCustomerId(), 10);
+	}
+	
+	@Test
+	public void testGetFirstName() {
+		assertEquals(c.getFirstName(), "Lilllie");
+	}
+	
+	@Test
+	public void testSetFirstName() {
+		c.setFirstName("Sally");
+		assertEquals(c.getFirstName(), "Sally");
+	}
+	
+	@Test
+	public void testGetLastName() {
+		assertEquals(c.getLastName(), "Godar");
+	}
+	
+	@Test
+	public void testSetLastName() {
+		c.setLastName("Smith");
+		assertEquals(c.getLastName(), "Smith");
+	}
+	
+	@Test
+	public void testGetEmail() {
+		assertEquals(c.getEmail(), "lgodar9@google.de");
+	}
+	
+	@Test
+	public void testSetEmail() {
+		c.setEmail("test@gmail.com");
+		assertEquals(c.getEmail(), "test@gmail.com");
+	}
+	
+	@Test
+	public void testGetPassword() {
+		assertEquals(c.getPassword(), "VnYAfTaj7E");
+	}
+	
+	@Test
+	public void testSetPassword() {
+		c.setPassword("password");
+		assertEquals(c.getPassword(), "password");
+	}
+	
+	@Test
+	public void testCustomerToString() {
+		assertEquals(c.toString(), "Customer [customerId=10, firstName=Lilllie, "
+				+ "lastName=Godar, email=lgodar9@google.de, password=VnYAfTaj7E]");
+	}
+	
 }
